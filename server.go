655fccf21f36a7440ecc2d179a4c8e04bad4f0ae
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-07-08 18:23:47 +0530

    Initial commit

---
+++ server.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+type todo struct {
+	Item string
+}
+
+func main() {
+	connStr := "postgresql://postgres:1795@localhost:5432/todos?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	// The fiber context object contains everything about the incoming request, like the headers, query string parameters, post body, etc.
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Static("/", "./public")
+
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+	rows, err := db.Query("SELECT * FROM todos")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
+
+	return c.Render("index", fiber.Map{
+		"Todos": todos,
+	})
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	newTodo := todo{}
+	if err := c.BodyParser(&newTodo); err != nil {
+		log.Printf("An error occured: %v", err)
+		return c.SendString(err.Error())
+	}
+
+	fmt.Printf("%v", newTodo)
+
+	// N.B., we use the db.Query() method whenever we expect a result from the database query and db.Exec() when we don’t. Again, refer to the SQL documentation for more info.
+
+	if newTodo.Item != "" {
+		_, err := db.Exec("INSERT INTO todos VALUES ($1)", newTodo.Item)
+		if err != nil {
+			log.Fatalf("An error occured while executing query: %v", err)
+		}
+	}
+
+	return c.Redirect("/")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	oldItem := c.Query("olditem")
+	newItem := c.Query("newitem")
+	db.Exec("UPDATE todos SET item=$1 WHERE item=$2", newItem, oldItem)
+	return c.Redirect("/")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	todoToDelete := c.Query("item")
+	db.Exec("DELETE FROM todos WHERE item=$1", todoToDelete)
+	return c.SendString("deleted")
+}
